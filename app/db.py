e110b4f422d6072f43a9a3107819610fa17c2108
Michael Lacroix <kennycodes@hotmail.com>
2025-09-13 14:03:16 -0400

    add config

--- app/db.py
+++ app/db.py
@@ -1,10 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from .config import settings
 
-# To change for .env file
-DATABASE_URL = "postgresql+psycopg2://postgres:password@localhost/mydb"
-
-engine = create_engine(DATABASE_URL, future=True)
+engine = create_engine(settings.DATABASE_URL, future=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
